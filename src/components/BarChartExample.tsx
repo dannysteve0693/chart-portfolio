aa345c6735469b026019db6e393a832d1c10394f
Danny <dannysteve0693@gmail.com>
2025-08-29 10:00:32 +0700

    Add color for Barchart

--- src/components/BarChartExample.tsx
+++ src/components/BarChartExample.tsx
@@ -1,30 +1,35 @@
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, LabelList, Cell } from 'recharts';
 
 function BarChartExample() {
-
   const data = [
-  {
-    "name": "Page A",
-    "uv": 30000,
-  },
-  {
-    "name": "Page B",
-    "uv": 15000,
-  },
-  
-]
-                           
+    {
+      "name": "Page A",
+      "uv": 30000,
+      "color": "#3b82f6"
+    },
+    {
+      "name": "Page B",
+      "uv": 15000,
+      "color": "#ef4444"
+    },
+  ]
 
   return (
     <div>
       <div className='mt-4'>
         <BarChart width={730} height={250} data={data} layout='vertical'>
           {/* <CartesianGrid strokeDasharray="3 3" /> */}
-          <XAxis  type='number' domain={[0, 35000]} label={{ value: "in Tonnes", position: "insideBottomRight", offset: -5 }} />
+          <XAxis type='number' domain={[0, 35000]} label={{ value: "in Tonnes", position: "insideBottomRight", offset: -5 }} />
           <YAxis dataKey="name" type='category' />
           <Tooltip />
           <Legend />
-          <Bar dataKey="uv" fill="#82ca9d"  />
+          <Bar dataKey="uv" fill="#82ca9d"
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={entry.color} />
+            ))}
+            <LabelList dataKey="uv" position="insideRight" fill="#fff" />
+          </Bar>
         </BarChart>
       </div>
       <div className='mt-4 px-4 py-5 '>
